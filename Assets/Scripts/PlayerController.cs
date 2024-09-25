f6503521f672e0ea0341bcc6b2e0f64d690e773c
Ltamulen12 <ltamulen12@gmail.com>
2024-09-25 12:22:15 -0400

    player control and shooting direction
    Added a way for the play to shoot in any direction that it is facing, added a detect collisions script to add to the walls and soon enemies.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,19 +10,32 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    private float horizontalInput;
+    public float horizontalInput;
+    public GameObject projectilePrefab;
+    public float speed = 15.0f;
+    private float turnSpeed =200.0f;
+    public float verticalInput;
+    public Transform projectileSpawnPoint;
     private float forwardInput;
-    private float speed = 10.0f;
-    private float turnSpeed = 200.0f;
+    public float BulletSpeed = 20.0f;
+
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
-
-       //moves the player move forward
+       horizontalInput = Input.GetAxis("Horizontal");
+       forwardInput = Input.GetAxis("Vertical");
+        //moves the player forward
        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-       // moves the player side to side
-       transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);    
+       // rotates the player 
+       transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime); 
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            GameObject projectile = Instantiate(projectilePrefab, projectileSpawnPoint.position, projectileSpawnPoint.rotation);
+
+            // Apply force to the projectile to move it forward
+            Rigidbody projectileRb = projectile.GetComponent<Rigidbody>();
+        }
     }
 }
